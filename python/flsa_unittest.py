a2b59296aa250566ee2345198136dbb8e3c90e1a
Yuma Takeda <utyuma013@g.ecc.u-tokyo.ac.jp>
2022-09-16 10:54:38 +0900

    bug fixed

--- python/flsa_unittest.py
+++ python/flsa_unittest.py
@@ -10,21 +10,6 @@ class ReturnTest(unittest.TestCase):
         beta = flsa_dp.main([0, 1], 0.5)
         self.assertTrue(beta == [0.5, 0.5])
 
-        args = sys.argv  # args: input, lam, output
-
-        fi = open(args[0], "r")
-        fo = open(args[2], "r")
-
-        input_data = fi.read()
-        output_data = fo.read()
-
-        fi.close()
-        fo.close()
-
-        beta = flsa_dp.main(np.array(input_data), args[1], "squared")
-        self.assertTrue(np.testing.assert_array_equal(
-            beta, np.array(output_data)))
-
     def test_from_dp_cpp(self):
         beta = flsa_dp.main(self.input_data, self.lam)
         np.testing.assert_array_almost_equal(
