a56228f9de9f7d59f30133a7819ff0524c21d10a
Yuma Takeda <utyuma013@g.ecc.u-tokyo.ac.jp>
2022-09-18 18:52:20 +0900

    first version of DeltaLogistic

--- python/flsa_dp.py
+++ python/flsa_dp.py
@@ -87,41 +87,7 @@ class DeltaLogistic(DeltaFunc):
         self.tangency_intervals = []
 
     def find_tangency(self, g):
-        for t in range(len(self.knots)-1):
-            if self.calc_derivative_at(self.knots[t+1], t) - g > 0:
-                self.tangency_intervals.append(t+1)
-                return self.calc_inverse_spline(t,g)
-        self.tangency_intervals.append(len(self.knots))
-        return np.inf
-
-    def copy(self):
-        new = DeltaLogistic()
-        new.knots = copy.copy(self.knots)
-        new.coef_list = copy.deepcopy(self.coef_list)
-        new.tangency_intervals = copy.copy(self.tangency_intervals)
-        return new
-
-    def overwrite(self, left_new_knot, right_new_knot, lamb):
-        tmp_knots = self.knots[self.tangency_intervals[0]:self.tangency_intervals[1]]
-        tmp_coef_list = self.coef_list[self.tangency_intervals[0]-1:self.tangency_intervals[1]]
-
-        if left_new_knot != -np.inf:
-            tmp_knots = [-np.inf, left_new_knot] + tmp_knots
-            tmp_coef_list = [self.get_constant_f(-lamb)] + tmp_coef_list
-        else:
-            tmp_knots = [-np.inf] + tmp_knots
-
-        if right_new_knot != np.inf:
-            tmp_knots = tmp_knots + [right_new_knot, np.inf]
-            tmp_coef_list = tmp_coef_list + [self.get_constant_f(lamb)]
-        else:
-            if tmp_knots[-1] != np.inf:
-                tmp_knots = tmp_knots + [np.inf]
-
-        self.knots = tmp_knots
-        self.coef_list = tmp_coef_list
-
-        self.tangency_intervals = []
+        pass
 
         return self
 
@@ -158,6 +124,7 @@ class DeltaLogistic(DeltaFunc):
 
 class DeltaSquared(DeltaFunc):
     basis_function_list = [lambda x: x, lambda x: 1]
+    tangency_intervals = []
 
     def __init__(
         self,
