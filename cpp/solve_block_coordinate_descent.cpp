4341fe2b6e7549cf295b1af094d2a95ffac84458
hiroyaiyori <vp054az116@gmail.com>
2022-11-03 13:42:57 +0900

    add calc loss function and convergence judge

--- cpp/solve_block_coordinate_descent.cpp
+++ cpp/solve_block_coordinate_descent.cpp
@@ -25,7 +25,7 @@ double clm_derivative(int q, double x, double *b, int y) {
     return 1 - sigmoid(b[y - 2] - x) - sigmoid(b[y - 1] - x);
 }
 
-void solve_square(int n, int q, double *f, double *b, double *y, double *solver_y) {
+void solve_square(int n, int q, double *f, double *b, int *y, double *solver_y) {
     double l_inv = 1/L;
 
     // calc solver_y
@@ -34,6 +34,22 @@ void solve_square(int n, int q, double *f, double *b, double *y, double *solver_
     }
     return;
 }
+
+double calc_loss(vector<double> fsum, vector<int> y, double *b, int q){
+    double loss;
+    for (size_t i=0; i < y.size(); i++){
+        if (y[i] == 1){
+            loss -= log(sigmoid(b[1] - fsum[i]));
+        }
+        else if (y[i] == q){
+            loss -= log(1 - sigmoid(b[q - 1] - fsum[i]));
+        }else {
+            loss -= log(sigmoid(b[y[i] - 1] - fsum[i]) - sigmoid(b[y[i] - 2] - fsum[i]));
+        }
+    }
+    return loss;
+}
+    
 
 void set_argsort(vector<vector<int>> &argsort, vector<deque<bool>> &argsort_c, vector<vector<int>> &argsort_inv, vector<vector<double>> const &x){
     int d = x.size();
@@ -56,11 +72,13 @@ void set_argsort(vector<vector<int>> &argsort, vector<deque<bool>> &argsort_c, v
         }
     }
     return;
-
 }
 
-void solve_block_coordinate_descent(vector< vector<double>> x, vector< vector<double>>f, vector<double> y , int q, double lam) {
+
+void solve_block_coordinate_descent(vector< vector<double>> x, vector< vector<double>>f, vector<int> y , int q, double lam) {
     double b[] = {-1, 0, 1};
+    double loss;
+    double lastloss;
     int n = y.size();
     int d = x.size();
     vector<double> fsum(n, 0);
@@ -70,6 +88,7 @@ void solve_block_coordinate_descent(vector< vector<double>> x, vector< vector<do
     vector<vector<int>> argsort_inv(d, vector<int>(n));
     set_argsort(argsort, argsort_c, argsort_inv, x);
     vector<double> solver_y(n);
+    vector<double> solver_f(n);
     vector<double> sorted_solver_y(n);
 
     while (1) {
@@ -80,10 +99,16 @@ void solve_block_coordinate_descent(vector< vector<double>> x, vector< vector<do
                 fsumtmp[i] = fsum[i] - f[j][i];
                 sorted_solver_y[i] = solver_y[argsort[j][i]];
             }
-            tf_dp(n, &y[0], lam, &argsort_c[j][0], &f[j][0]);
+            tf_dp(n, &sorted_solver_y[0], lam, &argsort_c[j][0], &solver_f[0]);
             for (int i = 0; i < n; i++){
+                f[j][i] = solver_f[argsort_inv[j][i]];
                 fsum[i] = fsumtmp[i] + f[j][i];
             }
+            loss = calc_loss(fsum, y, b, q);
+            if ((loss - lastloss) * (loss - lastloss) < EPS) {
+                break;
+            }
+            lastloss = loss;
         }
     }
 }
