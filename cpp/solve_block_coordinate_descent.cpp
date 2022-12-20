526b11ac1f4898a9faacfd4bc485d4854cf5ad1e
hiroyaiyori <vp054az116@gmail.com>
2022-12-20 18:06:51 +0900

    update

--- cpp/solve_block_coordinate_descent.cpp
+++ cpp/solve_block_coordinate_descent.cpp
@@ -15,9 +15,10 @@ using namespace std;
 using namespace Eigen;
 
 const double EPS = 1e-6;
-const int ITER = 1000000;
+const double PRE_FIT_EPS = 1e-4;
+const int ITER = 10000;
 const double LOG2 = 0.693147;
-const int pn = 500;
+const int pn = 100;
 const int pm = 10;
 
 double sigmoid(double x) {
@@ -34,7 +35,7 @@ double clm_w_derivative(int q, double x, double *b, int y) {
     return 1 - sigmoid(b[y - 2] - x) - sigmoid(b[y - 1] - x);
 }
 
-void clm_b_derivative(int n, int q, const double *f, const double *b, const int *y, VectorXd& db, MatrixXd& hessianb) {
+void clm_b_derivative(int n, int q, const double *f, const double *b, const int *y, const double M, VectorXd& db, MatrixXd& hessianb) {
     double sigmoid_1, sigmoid_2, b_dif_exp, inv_b_dif_exp, gamma;
     int yi;
     db = VectorXd::Zero(q-1);
@@ -44,15 +45,19 @@ void clm_b_derivative(int n, int q, const double *f, const double *b, const int 
         yi = y[i];
         if (yi == q) {
             sigmoid_2 = sigmoid(b[q - 2] - f[i]);
-            db(q - 2) += sigmoid_2;
-            // cout << q-2 <<":" <<sigmoid_2 << "->" << db(q-2) << " ";
-            hessianb(q-2,q-2) += sigmoid_2 * (1 - sigmoid_2);
+            b_dif_exp = exp(M - b[q - 2]);
+            inv_b_dif_exp = 1 / b_dif_exp;
+            gamma = 1 / (inv_b_dif_exp + b_dif_exp - 2);
+            db(q - 2) -= 1 - sigmoid_2 + 1 / (inv_b_dif_exp - 1);
+            hessianb(q-2,q-2) += sigmoid_2 * (1 - sigmoid_2) + gamma;
         }
         else if (yi == 1) {
             sigmoid_1 = sigmoid(b[0] - f[i]);
-            db(0) += sigmoid_1 - 1;
-            // cout << 0<<":"<< sigmoid_1 - 1 << "->" << db(0) <<" ";
-            hessianb(0, 0) += sigmoid_1 * (1 - sigmoid_1);
+            b_dif_exp = exp(b[0] + M);
+            inv_b_dif_exp = 1 / b_dif_exp;
+            gamma = 1 / (inv_b_dif_exp + b_dif_exp - 2);
+            db(0) -= 1 - sigmoid_1 + 1 / (b_dif_exp - 1);
+            hessianb(0, 0) += sigmoid_1 * (1 - sigmoid_1) + gamma;
         }
         else {
             sigmoid_1 = sigmoid(b[yi - 1] - f[i]);
@@ -61,16 +66,13 @@ void clm_b_derivative(int n, int q, const double *f, const double *b, const int 
             inv_b_dif_exp = 1 / b_dif_exp;
             gamma = 1 / (inv_b_dif_exp + b_dif_exp - 2);
             db(yi - 1) -= 1 - sigmoid_1 + 1 / (b_dif_exp - 1);
-            // cout << yi-1 <<":" << -(1 - sigmoid_1 + 1 / (b_dif_exp - 1))<<"->"<< db(yi - 1) << " ";
             db(yi - 2) -= 1 - sigmoid_2 + 1 / (inv_b_dif_exp - 1);
-            // cout << yi-2 <<":" << sigmoid_2 << "&"<< b[yi - 1] <<"-"<< b[yi - 2]<<"*"<<b_dif_exp << "&"<< -(1 - sigmoid_2 + 1 / (inv_b_dif_exp - 1))<<"->"<< db(yi - 2) << " ";
             hessianb(yi - 2, yi - 1) -= gamma;
             hessianb(yi - 1, yi - 2) -= gamma;
             hessianb(yi - 1, yi - 1) += sigmoid_1 * (1 - sigmoid_1) + gamma;
             hessianb(yi - 2, yi - 2) += sigmoid_2 * (1 - sigmoid_2) + gamma;
         }
     }
-    // cout << "\n";
     return;
 }
 
@@ -78,15 +80,11 @@ void clm_b_derivative(int n, int q, const double *f, const double *b, const int 
 
 void solve_square(const int n, const int q, double *fsum, double* f, double *b, const int *y, double *solver_y, int iter, const double l_inv) {
     double grad;
-    // cout << "solever y";
 
-    // calc solver_y
     for (int i = 0; i < n; i++){
         grad = clm_w_derivative(q, fsum[i], b, y[i]);
         solver_y[i] = f[i] - l_inv * grad;
-        // cout << clm_derivative(q, fsum[i], b, y[i])<< ";"<<solver_y[i] << ":" << f[i]<<" ";
     }
-    // cout << "\n";
     return;
 }
 
@@ -98,8 +96,8 @@ double log1pexpz(double z){
 }
 
 double calc_objective(vector<double> fsum, vector<int> y, double *b, int q, vector<vector<double>> f, vector<vector<int>> argsort, double lam){
+
     double loss;
-    // cout << "loss: ";
     for (size_t i=0; i < y.size(); i++){
         if (y[i] == 1){
             loss += log1pexpz(fsum[i] - b[0]);
@@ -114,9 +112,7 @@ double calc_objective(vector<double> fsum, vector<int> y, double *b, int q, vect
             else c1 = log1p(-exp(-delta));
             loss -= c1 - log1pexpz(-gamma-delta) - log1pexpz(gamma);
         }
-        // cout << loss << ":" << fsum[i]<<","<<y[i]<<"\t";
     }
-    // cout << "\n";
     for (size_t j=0; j < f.size(); j++){
         for (size_t i=0; i < y.size() - 1; i++){
             loss += lam * abs(f[j][argsort[j][i+1]] - f[j][argsort[j][i]]);
@@ -166,26 +162,20 @@ double calc_suboptibality(int q, int n, double *b, double *fsum, double *f, doub
     gsign = new int[n + 1];
     gsign[0] = 0;
     gsign[n] = 0;
-    // cout << "gsign: ";
     for (int i = 1; i < n;i++) {
         if (f[i - 1] > f[i]){
             gsign[i] = 1;
         }
-        // else if (abs(f[i - 1]-f[i]) < EPS/n){
         else if (f[i - 1]==f[i]){
             gsign[i] = 2;
         }
         else {
             gsign[i] = -1;
         }
-        // cout << gsign[i] << ",";
     }
-    // cout << "\n";
     int pre_i = 0;
-    // cout << "temp_loss: "<< endl;
     for (int i = 0; i < n-1; i++){
         temp_loss += clm_w_derivative(q, fsum[i], b, y[i]);
-        // cout << temp_loss << " ";
         if (i==n-1||!c[i]) {
             if (gsign[pre_i] < 2) {
                 temp_loss -= gsign[pre_i] * lam;
@@ -198,26 +188,21 @@ double calc_suboptibality(int q, int n, double *b, double *fsum, double *f, doub
             } else if (gsign[pre_i] == 2 || gsign[i + 1] == 2) {
                 temp_loss = calc_min_subopt(temp_loss, lam);
             }
-            // cout << " \n-> " <<temp_loss << " ";
-            // cout << pre_i <<"-"<< i << "/"<<n<< "\n";
             subopt += abs(temp_loss);
             for (int j = pre_i; j <= i; j++){
                 gradient[j] = temp_loss;
-
-                // cout <<j <<";"<<gradient[j] << " ";
             }
             pre_i = i + 1;
             temp_loss = 0;
         }
     }
-    // cout << "\n";
     return subopt;
 }
 
 
 
 
-void solve_block_coordinate_descent(const vector< vector<double>> x, vector< vector<double>>& f,const  vector<int> y , int q, double lam, double *b, double L) {
+void solve_block_coordinate_descent(const vector< vector<double>> x, vector< vector<double>>& f,const  vector<int> y , int q, double lam, double *b, double L, const double M) {
     int n = y.size();
     int d = x.size();
     double duration = 0;
@@ -228,10 +213,16 @@ void solve_block_coordinate_descent(const vector< vector<double>> x, vector< vec
     vector<vector<int>> argsort_c(d,vector<int>(n));
     vector<vector<int>> argsort_inv(d, vector<int>(n));
     set_argsort(argsort, argsort_c, argsort_inv, x);
+    vector<vector<int>> sorted_y(d, vector<int>(n));
+    for (int j = 0; j < d; j++){
+        for (int i = 0; i < n; i++){
+            // これは毎回やる必要はない
+            sorted_y[j][i] = y[argsort[j][i]];
+        }
+    }
     vector<double> solver_y(n);
     vector<double> solver_f(n);
     vector<double> sorted_solver_y(n);
-    vector<int> sorted_y(n);
     vector<double> sorted_fsum(n);
     vector<double> sorted_f(n);
     vector<vector<double>> gradient(d, vector<double>(n));
@@ -239,10 +230,11 @@ void solve_block_coordinate_descent(const vector< vector<double>> x, vector< vec
     MatrixXd hessianb(q-1,q-1);
     LLT<MatrixXd> llt;
     VectorXd newton(q-1);
+    double newton_eta;
 
     double loss;
 
-    double bsubopt;
+    double bsubopt, pre_bsubopt;
 
     double l_inv = 1/(L);
     double flsa_lam = l_inv * lam;
@@ -263,44 +255,80 @@ void solve_block_coordinate_descent(const vector< vector<double>> x, vector< vec
                 fsum[i] = fsumtmp[i] + f[j][i];
             }
         }
-
-        clm_b_derivative(n, q, &fsum[0], b, &y[0], db, hessianb);
-        llt.compute(hessianb);
-        newton = llt.solve(db);
-        for (int l = 0; l<q-1; l++){
-            b[l] -= newton[l];
-        }
         end = chrono::system_clock::now();
-        duration += chrono::duration_cast<std::chrono::seconds>(end-start).count(); 
+        duration += chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); 
         subopt = 0;
         for (int j = 0; j < d; j++){
             for (int i = 0; i < n; i++){
-                // これは毎回やる必要はない
-                sorted_y[i] = y[argsort[j][i]];
                 sorted_fsum[i] = fsum[argsort[j][i]];
                 sorted_f[i] = f[j][argsort[j][i]];
             }
-            subopt += calc_suboptibality(q, n, b, &sorted_fsum[0], &sorted_f[0],lam, argsort_c[j], &sorted_y[0], &gradient[j][0]);
+            subopt += calc_suboptibality(q, n, b, &sorted_fsum[0], &sorted_f[0],lam, argsort_c[j], &sorted_y[j][0], &gradient[j][0]);
         }
         subopt/=n*d;
+        if (k%pn==0){
+            loss = calc_objective(fsum, y, b, q, f, argsort, lam);
+            std::cout << "iter: " << k << " time: " << duration;
+            std::cout <<"  loss: " << loss <<" , subopt" << " : "<<subopt <<"\n";
+        }
+        
+        if (subopt < PRE_FIT_EPS) {
+            std::cout << "converged: " << k << "\n";
+            std::cout << duration << "\n";
+            break;
+        }
+    }
+    for (int k=0; k<ITER; k++) {
+        chrono::system_clock::time_point  start, end; // 型は auto で可
+        start = chrono::system_clock::now();
+        clm_b_derivative(n, q, &fsum[0], b, &y[0], M, db, hessianb);
+        llt.compute(hessianb);
+        newton = llt.solve(db);
+        
+        for (int l = 0; l<q-1; l++){
+        // for (int l = 1; l<q-2; l++){
+            b[l] -= newton[l];
+        }
+        for (int j = 0; j < d; j++){
+            solve_square(n, q, &fsum[0], &f[j][0], b, &y[0], &solver_y[0], k, l_inv);
+            for (int i = 0; i < n; i++){
+                fsumtmp[i] = fsum[i] - f[j][i];
+                sorted_solver_y[i] = solver_y[argsort[j][i]];
+            }
+            tf_dp(n, &sorted_solver_y[0], flsa_lam, &argsort_c[j][0], &solver_f[0]);
+            for (int i = 0; i < n; i++){
+                f[j][i] = solver_f[argsort_inv[j][i]];
+                fsum[i] = fsumtmp[i] + f[j][i];
+            }
+        }
+
+        end = chrono::system_clock::now();
+        duration += chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); 
+        subopt = 0;
         bsubopt = 0;
         for (int l = 0; l<q-1; l++){
             bsubopt += abs(db[l]);
         }
         bsubopt/=q-1;
+        for (int j = 0; j < d; j++){
+            for (int i = 0; i < n; i++){
+                sorted_fsum[i] = fsum[argsort[j][i]];
+                sorted_f[i] = f[j][argsort[j][i]];
+            }
+            subopt += calc_suboptibality(q, n, b, &sorted_fsum[0], &sorted_f[0],lam, argsort_c[j], &sorted_y[j][0], &gradient[j][0]);
+        }
+        subopt/=n*d;
+
 
         if (k%pn==0){
             loss = calc_objective(fsum, y, b, q, f, argsort, lam);
-            std::cout << "iter: " << k << "time: " << duration;
+            std::cout << "iter: " << k << " time: " << duration;
             std::cout <<"  loss: " << loss <<" , subopt" << " : "<<subopt << " , bsubopt" << " : "<<bsubopt << "\n";
             std::cout << "b: [";
             for (int l = 0; l<q-1; l++){
                 std::cout << b[l] << " ";
             }
             std::cout << "]\n";
-            // std::cout << db<<"\n";
-            // std::cout << hessianb <<"\n";
-            // std::cout << newton <<"\n";
         }
         
         if (subopt < EPS && bsubopt < EPS) {
@@ -344,18 +372,7 @@ double solve_gradient_descent(const vector< vector<double>> x, vector< vector<do
    
     vector<vector<double>> gradient(d, vector<double>(n));
     for (int k=0; k<ITER; k++) {
-        // if (k%pn==0){
-        //     std::cout << k <<",";
-        //     for (int i = 0; i < n; i++){
-        //         for (int j = 0; j < d; j++){
-        //             std::cout <<std::setprecision(10)<< f[j][i] << ",";
-        //         }
-        //     }
-        //     std::cout<<"\n";
-        // }
- 
         subopt= 0;
-        // cout << "fsum: ";
 
         chrono::system_clock::time_point  start, end; // 型は auto で可
         start = chrono::system_clock::now();
@@ -365,9 +382,7 @@ double solve_gradient_descent(const vector< vector<double>> x, vector< vector<do
             for (int j = 0; j < d; j++){
                 fsum[i] += f[j][i];
             }
-            // cout << fsum[i] << ", ";
         }
-        // cout << "\n";
         for (int j = 0; j < d; j++){
             for (int i = 0; i < n; i++){
                 sorted_y[i] = y[argsort[j][i]];
@@ -386,13 +401,6 @@ double solve_gradient_descent(const vector< vector<double>> x, vector< vector<do
             if (loss==INFINITY){
                 return loss;
             }
-            // std::cout << "iter: " << k;
-            // std::cout << "   loss: " << loss;
-            // std::cout << " . subopt" << " : "<<subopt << "\n";
-
-            // std::cout << k <<",";
-            // std::cout <<loss<< "\n";
- 
         }
         
         if (subopt < EPS) {
@@ -402,22 +410,8 @@ double solve_gradient_descent(const vector< vector<double>> x, vector< vector<do
 
         for (int j = 0; j < d; j++){
             for (int i = 0; i < n; i++){
-                // updated_f[j][i]  = f[j][i] - l_inv * gradient[j][argsort_inv[j][i]];
-                // if (i!=0 && (updated_f[j][i] - updated_f[j][i-1]) * (f[j][i] -f[j][i-1]) <=0 ) {
-                //     updated_f[j][i] = updated_f[j][i-1];
-                // }
-                // f[j][i] -= updated_f[j][i];
-                
-                // if (i!=n-1 && (f[j][i + 1]-f[j][i])* (f[j][i + 1] -f[j][i] + tmp_f) <=0 ) {
-                //     f[j][i] = f[j][i+1];
-                //     continue;
-                // }
-
                 f[j][i] -= steepest_linv / sqrt(t0 + k) * gradient[j][argsort_inv[j][i]];
-                // cout <<i<<argsort_inv[j][i]<<gradient[j][argsort_inv[j][i]]<<f[j][i] << " ";
-                // cout <<f[j][i] << " ";
             }
-            // cout <<  "\n";
         }
         // cout << "loss: " << loss;
         // cout << " . subopt" << " : "<<subopt << "\n";
@@ -427,3 +421,9 @@ double solve_gradient_descent(const vector< vector<double>> x, vector< vector<do
     // cout << "duration: " << duration << "\n";
     return loss;
 }
+
+// PYBIND11_PLUGIN(flsaclm) {
+//     py::module m("flsaclm", "flsaclm made by pybind11");
+//     m.def("solve_block_coordinate_descent", &solve_block_coordinate_descent);
+//     return m.ptr();
+// }
--- cpp/test_solve_block_coordinate_descent.cpp
+++ cpp/test_solve_block_coordinate_descent.cpp
@@ -87,14 +87,23 @@ void print(std::vector<T> const &input, std::vector<int> const &index)
 
 int main(){
     double yi;
+
     string dataname = "wlb";
     std::string filename = "/home/iyori/work/gam/datasets/"+dataname+"/train_"+dataname+".2";
-    // std::string filename = "/home/iyori/work/gam/experiments/datasets/ordinal-regression/"+dataname+"/matlab/train_"+dataname+".1";
+    std::string test_filename = "/home/iyori/work/gam/datasets/"+dataname+"/test_"+dataname+".2";
+
+
+    // string dataname = "winequality-red";
+    // std::string filename = "/home/iyori/work/gam/experiments/datasets/ordinal-regression/"+dataname+"/matlab/train_"+dataname+".5";
+    // std::string test_filename = "/home/iyori/work/gam/experiments/datasets/ordinal-regression/"+dataname+"/matlab/test_"+dataname+".5";
+
     // std::string filename = "/home/iyori/work/gam/experiments/datasets/discretized-regression/5bins/"+dataname+"/matlab/train_"+dataname+".1";
 
-    vector<vector<double>> data = csv2vector(filename);
-    int d = data[0].size() - 1;
-    int n = data.size();
+    vector<vector<double>> train_data = csv2vector(filename);
+    int d = train_data[0].size() - 1;
+    // int n = train_data.size();
+    int n = train_data.size();
+    double M = 10;
     vector<vector<double>> x(d,vector<double>{});
     vector<int> y{};
     for (int i=0; i<n; i++){
@@ -102,24 +111,47 @@ int main(){
         //     throw std::invalid_argument("received negative value");
         // }
         for (size_t j=0; j<d; j++){
-            x[j].push_back(data[i][j]);
+            x[j].push_back(train_data[i][j]);
         }
-        y.push_back((int) data[i][d] + 1);
+        y.push_back((int) train_data[i][d]);
     }
+    // read test data;
+    vector<vector<double>> test_data = csv2vector(test_filename);
+    int test_n = test_data.size();
+    vector<vector<double>> test_x(d,vector<double>{});
+    vector<int> test_y{};
+    for (int i=0; i<test_n; i++){
+        // if (i.size()!=d+1){
+        //     throw std::invalid_argument("received negative value");
+        // }
+        for (size_t j=0; j<d; j++){
+            test_x[j].push_back(test_data[i][j]);
+        }
+        test_y.push_back((int) test_data[i][d]);
+    }
+    std::cout << "test_n: " << test_n << ", n: " << n << "\n";
 
     int q = *max_element(y.begin(), y.end());
     double b[q - 1];
-    std::iota(b, b + q - 1, -(double)q/2.0+1);
-    double lam = 0.1;
+    std::cout << "b: ";
+    for (int i=0; i < q-1; i++){
+        // b[i] = 2 * M / q * (i + 1) - M;
+        b[i] = 4 * i - 2 * (q - 2);
+        std::cout << b[i] << " ";
+    }
+    std::cout << "\n";
+    double lam = 0.4;
     vector<vector<double>> f(d, vector<double>(n));
     vector<vector<double>> f1(d, vector<double>(n));
+    vector<vector<double>> sortedf(d, vector<double>(n));
+    vector<vector<double>> sortedx(d, vector<double>(n));
     vector<double> fsum(n, 0);
     vector<double> fsumtmp(n, 0);
     vector<vector<int>> argsort(d, vector<int>(n));
     vector<vector<int>> argsort_c(d, vector<int>(n-1));
     vector<vector<int>> argsort_inv(d, vector<int>(n));
 
-    // set_argsort(argsort, argsort_c, argsort_inv, x);
+    set_argsort(argsort, argsort_c, argsort_inv, x);
 
     // for (int j = 0; j < d; j++){
     //     print(y, argsort[j]);
@@ -134,26 +166,121 @@ int main(){
     //     print(argsort_inv[j]);
     // }
 
-    solve_block_coordinate_descent(x, f, y, q, lam, b, 0.5);
+    solve_block_coordinate_descent(x, f, y, q, lam, b, 0.3, M);
     std::cout <<"_______________________________________________\n";
-    std::cout << "b: \n";
+    std::cout << "b: ";
     for (int l = 0; l<q-1; l++){
         std::cout << b[l] << " ";
     }
     std::cout << "\n";
- 
-    std::cout << "f: \n";
-    for (int j = 0; j<d; j++){
-        for (int i = 0; i<n; i++){
-        std::cout << f[j][i] << " ";
-        }
-        std::cout << "\n";
-    }
+
+    std::cout <<"_______________________________________________\n";
+    double f_train;
+    double qprob, qprobmax, tmp;
+    int pred;
+    double prob = 0;
+    double acc = 0;
+    double mae = 0;
 
     
-    double min_loss = INFINITY;
-    double loss;
-    double minL, mint0;
- 
+    for (int k = 0; k < n; k++){
+        tmp = 0;
+        for (int j = 0; j< d;j++){
+            tmp += f[j][k];
+        }
+    }
+
+    for (int j = 0; j< d;j++){
+        for (int k = 0; k < n; k++){
+            sortedf[j][k] = f[j][argsort[j][k]];
+            sortedx[j][k] = x[j][argsort[j][k]];
+        }
+    }
+
+    for (int k = 0; k < n; k++){
+        qprobmax = 0;
+        f_train = 0;
+        pred = 0;
+
+        for (int j = 0; j< d; j++){
+            // cout << x[j][argsort[j][n-1]] << ':' << test_x[j][k]<< " ";
+            // cout << x[j][k] << ":" << sortedx[j][n-1] << "; "<< f_train << " # " ;
+            if (sortedx[j][n-1] < x[j][k]) f_train+=sortedf[j][n-1];
+            else{
+
+                for (int i = 0; i< n;i++){
+                    if (sortedx[j][i] >= x[j][k]) 
+                    {
+                        f_train+=sortedf[j][i];
+                        // cout << sortedf[j][i] << " || ";
+                        break;
+                    }
+                }
+            }
+        }
+        // cout << "\n" << f_train << "\n";
+        for (int l = 1; l<=q; l++){
+            if (l==1) qprob = sigmoid(b[0] - f_train) - sigmoid(- f_train);
+            else if (l<q) qprob = sigmoid(b[l - 1] - f_train) - sigmoid(b[l - 2]- f_train);
+            else qprob = sigmoid(M - f_train) - sigmoid(b[q - 2]- f_train);
+            if (l == y[k]) prob += qprob;
+            if (qprobmax < qprob){
+                qprobmax = qprob;
+                pred = l;
+            }
+        }
+
+        // if (k < 5) cout << f_train << ": " << pred << "; " <<y[k]<<"\n______________________________________________\n";
+        // cout << "f: " << f_train << ", pred: " << pred << ", y: " << y[k] << "\n";
+
+        if (pred==y[k]) acc+=1;
+        mae+=abs(pred-y[k]);
+    }
+    std::cout << "PROB: " << prob/n <<"\n";
+    std::cout << "ACC: " << acc/n <<"\n";
+    std::cout << "MAE: " << mae/n <<"\n";
+
+
+    double f_test;
+    // double qprob, qprobmax;
+    // int pred;
+    acc = 0;
+    mae = 0;
+    prob = 0;
+    for (int k = 0; k < test_n; k++){
+        qprobmax = 0;
+        f_test = 0;
+        pred = 0;
+
+        for (int j = 0; j< d; j++){
+            if (sortedx[j][n-1] < test_x[j][k]) f_test+=sortedf[j][n-1];
+            else{
+
+                for (int i = 0; i< n;i++){
+                    if (sortedx[j][i] >= test_x[j][k]) 
+                    {
+                        f_test+=sortedf[j][i];
+                        break;
+                    }
+                }
+            }
+        }
+        for (int l = 1; l<=q; l++){
+            if (l==1) qprob = sigmoid(b[0] - f_test) - sigmoid(- f_test);
+            else if (l<q) qprob = sigmoid(b[l - 1] - f_test) - sigmoid(b[l - 2]- f_test);
+            else qprob = sigmoid(M - f_test) - sigmoid(b[q - 2]- f_test);
+            if (l == test_y[k]) prob += qprob;
+            if (qprobmax < qprob){
+                qprobmax = qprob;
+                pred = l;
+            }
+        }
+        if (pred==test_y[k]) acc+=1;
+        mae += std::abs(pred - test_y[k]);
+    }
+    std::cout << "PROB: " << prob/test_n <<"\n";
+    std::cout << "ACC: " << acc/test_n <<"\n";
+    std::cout << "MAE: " << mae/test_n <<"\n";
+
 
 }
